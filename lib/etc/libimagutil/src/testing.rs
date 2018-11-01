7f0d5b8bfd866b61c5671952e25839764ec337ed
Matthias Beyer <mail@beyermatthias.de>
2018-11-01 13:05:07 +0100

    Do not pass --rtp in test runtime

--- lib/etc/libimagutil/src/testing.rs
+++ lib/etc/libimagutil/src/testing.rs
@@ -96,8 +96,7 @@ macro_rules! make_mock_app {
 
             #[allow(unused)]
             pub fn generate_test_runtime<'a>(mut args: Vec<&'static str>) -> Result<Runtime<'a>, Error> {
-                let mut cli_args = vec![$appname, "--rtp", "/tmp"];
-
+                let mut cli_args = vec![$appname];
                 cli_args.append(&mut args);
                 debug!("Generating test runtime with args = {:?}", cli_args);
 
@@ -109,8 +108,7 @@ macro_rules! make_mock_app {
             pub fn reset_test_runtime<'a>(mut args: Vec<&'static str>, old_runtime: Runtime)
                 -> Result<Runtime<'a>, Error>
             {
-                let mut cli_args = vec![$appname, "--rtp", "/tmp"];
-
+                let mut cli_args = vec![$appname];
                 cli_args.append(&mut args);
                 debug!("New arguments: {:?}", cli_args);
 
