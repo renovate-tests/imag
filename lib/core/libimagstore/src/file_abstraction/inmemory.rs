renovate-tests/imag

commit 09e8619cf5e9667b8c252e379faa98972218fe24

libimagstore: Move from error-chain to failure
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

lib/core/libimagstore/src/file_abstraction/inmemory.rs
@@ -18,14 +18,16 @@
 //
 
 use std::path::PathBuf;
-
-use error::StoreError as SE;
-use error::StoreErrorKind as SEK;
 use std::collections::HashMap;
 use std::sync::Mutex;
 use std::cell::RefCell;
 use std::sync::Arc;
 use std::ops::Deref;
+
+use libimagerror::errors::ErrorMsg as EM;
+
+use failure::Fallible as Result;
+use failure::Error;
 
 use super::FileAbstraction;
 use super::FileAbstractionInstance;
@@ -62,21 +64,21 @@ impl FileAbstractionInstance for InMemoryFileAbstractionInstance {
     /**
      * Get the mutable file behind a InMemoryFileAbstraction object
      */
-    fn get_file_content(&mut self, _: StoreId) -> Result<Entry, SE> {
+    fn get_file_content(&mut self, _: StoreId) -> Result<Option<Entry>> {
         debug!("Getting lazy file: {:?}", self);
 
         self.fs_abstraction
             .lock()
-            .map_err(|_| SE::from_kind(SEK::LockError))
-            .and_then(|mut mtx| {
+            .map_err(|_| Error::from(EM::LockError))
+            .map(|mut mtx| {
                 mtx.get_mut()
                     .get(&self.absent_path)
                     .cloned()
-                    .ok_or_else(|| SE::from_kind(SEK::FileNotFound))
             })
+            .map_err(Error::from)
     }
 
-    fn write_file_content(&mut self, buf: &Entry) -> Result<(), SE> {
+    fn write_file_content(&mut self, buf: &Entry) -> Result<()> {
         match *self {
             InMemoryFileAbstractionInstance { ref absent_path, .. } => {
                 let mut mtx = self.fs_abstraction.lock().expect("Locking Mutex failed");
@@ -99,18 +101,19 @@ impl InMemoryFileAbstraction {
         &self.virtual_filesystem
     }
 
-    fn backend_cloned<'a>(&'a self) -> Result<HashMap<PathBuf, Entry>, SE> {
+    fn backend_cloned<'a>(&'a self) -> Result<HashMap<PathBuf, Entry>> {
         self.virtual_filesystem
             .lock()
-            .map_err(|_| SE::from_kind(SEK::LockError))
+            .map_err(|_| Error::from(EM::LockError))
             .map(|mtx| mtx.deref().borrow().clone())
+            .into()
     }
 
 }
 
 impl FileAbstraction for InMemoryFileAbstraction {
 
-    fn remove_file(&self, path: &PathBuf) -> Result<(), SE> {
+    fn remove_file(&self, path: &PathBuf) -> Result<()> {
         debug!("Removing: {:?}", path);
         self.backend()
             .lock()
@@ -118,43 +121,43 @@ impl FileAbstraction for InMemoryFileAbstraction {
             .get_mut()
             .remove(path)
             .map(|_| ())
-            .ok_or_else(|| SE::from_kind(SEK::FileNotFound))
+            .ok_or_else(|| EM::FileNotFound.into())
     }
 
-    fn copy(&self, from: &PathBuf, to: &PathBuf) -> Result<(), SE> {
+    fn copy(&self, from: &PathBuf, to: &PathBuf) -> Result<()> {
         debug!("Copying : {:?} -> {:?}", from, to);
         let mut mtx = self.backend().lock().expect("Locking Mutex failed");
         let backend = mtx.get_mut();
 
-        let a = backend.get(from).cloned().ok_or_else(|| SE::from_kind(SEK::FileNotFound))?;
+        let a = backend.get(from).cloned().ok_or_else(|| EM::FileNotFound)?;
         backend.insert(to.clone(), a);
         debug!("Copying: {:?} -> {:?} worked", from, to);
         Ok(())
     }
 
-    fn rename(&self, from: &PathBuf, to: &PathBuf) -> Result<(), SE> {
+    fn rename(&self, from: &PathBuf, to: &PathBuf) -> Result<()> {
         debug!("Renaming: {:?} -> {:?}", from, to);
         let mut mtx = self.backend().lock().expect("Locking Mutex failed");
         let backend = mtx.get_mut();
 
-        let a = backend.remove(from).ok_or_else(|| SE::from_kind(SEK::FileNotFound))?;
+        let a = backend.get(from).cloned().ok_or_else(|| EM::FileNotFound)?;
         backend.insert(to.clone(), a);
         debug!("Renaming: {:?} -> {:?} worked", from, to);
         Ok(())
     }
 
-    fn create_dir_all(&self, _: &PathBuf) -> Result<(), SE> {
+    fn create_dir_all(&self, _: &PathBuf) -> Result<()> {
         Ok(())
     }
 
-    fn exists(&self, pb: &PathBuf) -> Result<bool, SE> {
+    fn exists(&self, pb: &PathBuf) -> Result<bool> {
         let mut mtx = self.backend().lock().expect("Locking Mutex failed");
         let backend = mtx.get_mut();
 
         Ok(backend.contains_key(pb))
     }
 
-    fn is_file(&self, pb: &PathBuf) -> Result<bool, SE> {
+    fn is_file(&self, pb: &PathBuf) -> Result<bool> {
         // Because we only store Entries in the memory-internal backend, we only have to check for
         // existance here, as if a path exists in the inmemory storage, it is always mapped to an
         // entry. hence it is always a path to a file
@@ -165,13 +168,15 @@ impl FileAbstraction for InMemoryFileAbstraction {
         Box::new(InMemoryFileAbstractionInstance::new(self.backend().clone(), p))
     }
 
-    fn drain(&self) -> Result<Drain, SE> {
+    fn drain(&self) -> Result<Drain> {
         self.backend_cloned().map(Drain::new)
     }
 
-    fn fill<'a>(&'a mut self, mut d: Drain) -> Result<(), SE> {
+    fn fill<'a>(&'a mut self, mut d: Drain) -> Result<()> {
         debug!("Draining into : {:?}", self);
-        let mut mtx = self.backend().lock().map_err(|_| SEK::LockError)?;
+        let mut mtx = self.backend()
+            .lock()
+            .map_err(|_| EM::LockError)?;
         let backend = mtx.get_mut();
 
         for (path, element) in d.iter() {
@@ -182,17 +187,17 @@ impl FileAbstraction for InMemoryFileAbstraction {
         Ok(())
     }
 
-    fn pathes_recursively(&self, _basepath: PathBuf, storepath: PathBuf, backend: Arc<FileAbstraction>) -> Result<PathIterator, SE> {
+    fn pathes_recursively(&self, _basepath: PathBuf, storepath: PathBuf, backend: Arc<FileAbstraction>) -> Result<PathIterator> {
         trace!("Building PathIterator object (inmemory implementation)");
         let keys : Vec<PathBuf> = self
             .backend()
             .lock()
-            .map_err(|_| SE::from_kind(SEK::FileError))?
+            .map_err(|_| EM::LockError)?
             .get_mut()
             .keys()
             .map(PathBuf::from)
             .map(Ok)
-            .collect::<Result<_, SE>>()?; // we have to collect() because of the lock() above.
+            .collect::<Result<_>>()?; // we have to collect() because of the lock() above.
 
         Ok(PathIterator::new(Box::new(InMemPathIterBuilder(keys)), storepath, backend))
     }
@@ -201,7 +206,7 @@ impl FileAbstraction for InMemoryFileAbstraction {
 pub(crate) struct InMemPathIterBuilder(Vec<PathBuf>);
 
 impl PathIterBuilder for InMemPathIterBuilder {
-    fn build_iter(&self) -> Box<Iterator<Item = Result<PathBuf, SE>>> {
+    fn build_iter(&self) -> Box<Iterator<Item = Result<PathBuf>>> {
         Box::new(self.0.clone().into_iter().map(Ok))
     }
 

lib/core/libimagstore/Cargo.toml
@@ -29,8 +29,8 @@ walkdir = "2"
 is-match = "0.1"
 serde = "1"
 serde_json = "1"
-error-chain = "0.12"
-toml-query = "0.7"
+toml-query = { git = "https://github.com/matthiasbeyer/toml-query", branch = "failure" }
+failure        = "0.1"
 
 libimagerror = { version = "0.9.0", path = "../../../lib/core/libimagerror" }
 libimagutil  = { version = "0.9.0", path = "../../../lib/etc/libimagutil" }

lib/core/libimagstore/src/configuration.rs
@@ -19,9 +19,11 @@
 
 use toml::Value;
 
-use store::Result;
-use error::StoreError as SE;
-use error::StoreErrorKind as SEK;
+use failure::Fallible as Result;
+use failure::ResultExt;
+use failure::Error;
+
+use libimagerror::errors::ErrorMsg as EM;
 
 /// Checks whether the store configuration has a key "implicit-create" which maps to a boolean
 /// value. If that key is present, the boolean is returned, otherwise false is returned.
@@ -31,7 +33,10 @@ pub fn config_implicit_store_create_allowed(config: &Option<Value>) -> Result<bo
     let key = "store.implicit-create";
 
     if let Some(ref t) = *config {
-        t.read_bool(key)?.ok_or_else(|| SE::from_kind(SEK::ConfigKeyMissingError(key)))
+        t.read_bool(key)
+            .map_err(Error::from)
+            .context(EM::TomlQueryError)?
+            .ok_or_else(|| format_err!("Config key missing: {}", key))
     } else {
         Ok(false)
     }

lib/core/libimagstore/src/error.rs
@@ -1,253 +1,0 @@
-//
-// imag - the personal information management suite for the commandline
-// Copyright (C) 2015-2018 Matthias Beyer <mail@beyermatthias.de> and contributors
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; version
-// 2.1 of the License.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-//
-
-use std::path::PathBuf;
-
-use storeid::StoreId;
-
-error_chain! {
-    types {
-        StoreError, StoreErrorKind, ResultExt, Result;
-    }
-
-    foreign_links {
-        Io(::std::io::Error);
-        Fmt(::std::fmt::Error);
-        TomlDeserError(::toml::de::Error);
-        TomlSerError(::toml::ser::Error);
-        GlobPatternError(::glob::PatternError);
-        TomlQueryError(::toml_query::error::Error);
-    }
-
-    errors {
-
-        ConfigurationError      {
-            description("Store Configuration Error")
-            display("Store Configuration Error")
-        }
-
-        ConfigTypeError(key: &'static str, expected: &'static str) {
-            description("Store configuration type error")
-            display("Store configuration type error at '{}', expected {}", key, expected)
-        }
-
-        ConfigKeyMissingError(key: &'static str) {
-            description("Configuration Key missing")
-            display("Configuration Key missing: '{}'", key)
-        }
-
-        VersionError            {
-            description("Incompatible store versions detected")
-            display("Incompatible store versions detected")
-        }
-
-        CreateStoreDirDenied    {
-            description("Creating store directory implicitely denied")
-            display("Creating store directory implicitely denied")
-        }
-
-        FileError               {
-            description("File Error")
-            display("File Error")
-        }
-
-        IoError                 {
-            description("IO Error")
-            display("IO Error")
-        }
-
-        IdLocked                {
-            description("ID locked")
-            display("ID locked")
-        }
-
-        IdNotFound(sid: StoreId) {
-            description("ID not found")
-            display("ID not found: {}", sid)
-        }
-
-        FileNotFound            {
-            description("File corresponding to ID not found")
-            display("File corresponding to ID not found")
-        }
-
-        FileNotCreated          {
-            description("File corresponding to ID could not be created")
-            display("File corresponding to ID could not be created")
-        }
-
-        FileNotWritten          {
-            description("File corresponding to ID could not be written to")
-            display("File corresponding to ID could not be written to")
-        }
-
-        FileNotSeeked           {
-            description("File corresponding to ID could not be seeked")
-            display("File corresponding to ID could not be seeked")
-        }
-
-        FileNotRemoved          {
-            description("File corresponding to ID could not be removed")
-            display("File corresponding to ID could not be removed")
-        }
-
-        FileNotRenamed          {
-            description("File corresponding to ID could not be renamed")
-            display("File corresponding to ID could not be renamed")
-        }
-
-        FileNotCopied           {
-            description("File could not be copied")
-            display("File could not be copied")
-        }
-
-        DirNotCreated           {
-            description("Directory/Directories could not be created")
-            display("Directory/Directories could not be created")
-        }
-
-        StorePathExists(pb: PathBuf) {
-            description("Store path exists")
-            display("Store path exists: {:?}", pb)
-        }
-
-        StorePathCreate(pb: PathBuf) {
-            description("Store path create")
-            display("Store path create: {:?}", pb)
-        }
-
-        LockError               {
-            description("Error locking datastructure")
-            display("Error locking datastructure")
-        }
-
-        LockPoisoned            {
-            description("The internal Store Lock has been poisoned")
-            display("The internal Store Lock has been poisoned")
-        }
-
-        EntryAlreadyBorrowed(id: StoreId) {
-            description("Entry is already borrowed")
-            display("Entry is already borrowed: {:?}", id)
-        }
-
-        EntryAlreadyExists(id: StoreId) {
-            description("Entry already exists")
-            display("Entry already exists: {:?}", id)
-        }
-
-        MalformedEntry          {
-            description("Entry has invalid formatting, missing header")
-            display("Entry has invalid formatting, missing header")
-        }
-
-        HeaderTypeFailure       {
-            description("Header type is wrong")
-            display("Header type is wrong")
-        }
-
-        EncodingError           {
-            description("Encoding error")
-            display("Encoding error")
-        }
-
-        EntryRenameError(old: PathBuf, new: PathBuf) {
-            description("Entry rename error")
-            display("Entry rename error: {:?} -> {:?}", old, new)
-        }
-
-        StoreIdHandlingError    {
-            description("StoreId handling error")
-            display("StoreId handling error")
-        }
-
-        StoreIdLocalPartAbsoluteError(pb: PathBuf) {
-            description("StoreId 'id' part is absolute (starts with '/') which is not allowed")
-            display("StoreId 'id' part is absolute (starts with '/') which is not allowed: {:?}", pb)
-        }
-
-        StoreIdBuildFromFullPathError {
-            description("Building StoreId from full file path failed")
-            display("Building StoreId from full file path failed")
-        }
-
-        StoreIdHasNoBaseError(pb: PathBuf) {
-            description("StoreId has no 'base' part")
-            display("StoreId has no 'base' part: {:?}", pb)
-        }
-
-        CreateCallError(sid: StoreId) {
-            description("Error when calling create()")
-            display("Error when calling create({:?})", sid)
-        }
-
-        RetrieveCallError(sid: StoreId) {
-            description("Error when calling retrieve()")
-            display("Error when calling retrieve({:?})", sid)
-        }
-
-        GetCallError(sid: StoreId) {
-            description("Error when calling get()")
-            display("Error when calling get({:?})", sid)
-        }
-
-        UpdateCallError(sid: StoreId) {
-            description("Error when calling update()")
-            display("Error when calling update({:?})", sid)
-        }
-
-        RetrieveCopyCallError(sid: StoreId) {
-            description("Error when calling retrieve_copy()")
-            display("Error when calling retrieve_copy({:?})", sid)
-        }
-
-        DeleteCallError(sid: StoreId) {
-            description("Error when calling delete()")
-            display("Error when calling delete({:?})", sid)
-        }
-
-        MoveCallError(old: StoreId, new: StoreId) {
-            description("Error when calling move()")
-            display("Error when calling move({:?} -> {:?})", old, new)
-        }
-
-        // Parser-related errors
-
-        MissingMainSection  {
-            description("Missing main section")
-            display("Missing main section")
-        }
-
-        MissingVersionInfo  {
-            description("Missing version information in main section")
-            display("Missing version information in main section")
-        }
-
-        NonTableInBaseTable {
-            description("A non-table was found in the base table")
-            display("A non-table was found in the base table")
-        }
-
-        HeaderInconsistency {
-            description("The header is inconsistent")
-            display("The header is inconsistent")
-        }
-    }
-}
-

lib/core/libimagstore/src/file_abstraction/fs.rs
@@ -22,8 +22,7 @@ use std::io::{Seek, SeekFrom, Read};
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
-use error::{StoreError as SE, StoreErrorKind as SEK};
-use error::ResultExt;
+use libimagerror::errors::ErrorMsg as EM;
 
 use super::FileAbstraction;
 use super::FileAbstractionInstance;
@@ -34,6 +33,9 @@ use file_abstraction::iter::PathIterator;
 use file_abstraction::iter::PathIterBuilder;
 
 use walkdir::WalkDir;
+use failure::ResultExt;
+use failure::Fallible as Result;
+use failure::Error;
 
 #[derive(Debug)]
 pub struct FSFileAbstractionInstance(PathBuf);
@@ -43,34 +45,41 @@ impl FileAbstractionInstance for FSFileAbstractionInstance {
     /**
      * Get the content behind this file
      */
-    fn get_file_content(&mut self, id: StoreId) -> Result<Entry, SE> {
+    fn get_file_content(&mut self, id: StoreId) -> Result<Option<Entry>> {
         debug!("Getting lazy file: {:?}", self);
 
-        let mut file = open_file(&self.0)
-            .chain_err(|| SEK::FileNotFound)?;
+        let mut file = match open_file(&self.0) {
+            Err(err)       => return Err(Error::from(err)),
+            Ok(None)       => return Ok(None),
+            Ok(Some(file)) => file,
+        };
 
-        file.seek(SeekFrom::Start(0)).chain_err(|| SEK::FileNotSeeked)?;
+        file.seek(SeekFrom::Start(0)).context(EM::FileNotSeeked)?;
 
         let mut s = String::new();
 
         file.read_to_string(&mut s)
-            .chain_err(|| SEK::IoError)
+            .context(EM::IO)
+            .map_err(Error::from)
             .map(|_| s)
-            .and_then(|s| Entry::from_str(id, &s))
+            .and_then(|s: String| Entry::from_str(id, &s))
+            .map(Some)
     }
 
     /**
      * Write the content of this file
      */
-    fn write_file_content(&mut self, buf: &Entry) -> Result<(), SE> {
+    fn write_file_content(&mut self, buf: &Entry) -> Result<()> {
         use std::io::Write;
 
         let buf      = buf.to_str()?.into_bytes();
-        let mut file = create_file(&self.0).chain_err(|| SEK::FileNotCreated)?;
+        let mut file = create_file(&self.0).context(EM::FileNotCreated)?;
 
-        file.seek(SeekFrom::Start(0)).chain_err(|| SEK::FileNotCreated)?;
-        file.set_len(buf.len() as u64).chain_err(|| SEK::FileNotWritten)?;
-        file.write_all(&buf).chain_err(|| SEK::FileNotWritten)
+        file.seek(SeekFrom::Start(0)).context(EM::FileNotCreated)?;
+        file.set_len(buf.len() as u64).context(EM::FileNotWritten)?;
+        file.write_all(&buf)
+            .context(EM::FileNotWritten)
+            .map_err(Error::from)
     }
 }
 
@@ -82,19 +91,24 @@ pub struct FSFileAbstraction {}
 
 impl FileAbstraction for FSFileAbstraction {
 
-    fn remove_file(&self, path: &PathBuf) -> Result<(), SE> {
-        remove_file(path).chain_err(|| SEK::FileNotRemoved)
+    fn remove_file(&self, path: &PathBuf) -> Result<()> {
+        remove_file(path)
+            .context(EM::FileNotRemoved)
+            .map_err(Error::from)
     }
 
-    fn copy(&self, from: &PathBuf, to: &PathBuf) -> Result<(), SE> {
-        copy(from, to).chain_err(|| SEK::FileNotCopied).map(|_| ())
+    fn copy(&self, from: &PathBuf, to: &PathBuf) -> Result<()> {
+        copy(from, to)
+            .map(|_| ())
+            .context(EM::FileNotCopied)
+            .map_err(Error::from)
     }
 
-    fn rename(&self, from: &PathBuf, to: &PathBuf) -> Result<(), SE> {
+    fn rename(&self, from: &PathBuf, to: &PathBuf) -> Result<()> {
         match to.parent() {
             Some(p) => if !p.exists() {
                 debug!("Creating: {:?}", p);
-                let _ = create_dir_all(&PathBuf::from(p))?;
+                let _ = create_dir_all(&PathBuf::from(p)).context(EM::DirNotCreated)?;
             },
             None => {
                 debug!("Failed to find parent. This looks like it will fail now");
@@ -103,19 +117,23 @@ impl FileAbstraction for FSFileAbstraction {
         }
 
         debug!("Renaming {:?} to {:?}", from, to);
-        rename(from, to).chain_err(|| SEK::FileNotRenamed)
+        rename(from, to)
+            .context(EM::FileNotRenamed)
+            .map_err(Error::from)
     }
 
-    fn create_dir_all(&self, path: &PathBuf) -> Result<(), SE> {
+    fn create_dir_all(&self, path: &PathBuf) -> Result<()> {
         debug!("Creating: {:?}", path);
-        create_dir_all(path).chain_err(|| SEK::DirNotCreated)
+        create_dir_all(path)
+            .context(EM::DirNotCreated)
+            .map_err(Error::from)
     }
 
-    fn exists(&self, path: &PathBuf) -> Result<bool, SE> {
+    fn exists(&self, path: &PathBuf) -> Result<bool> {
         Ok(path.exists())
     }
 
-    fn is_file(&self, path: &PathBuf) -> Result<bool, SE> {
+    fn is_file(&self, path: &PathBuf) -> Result<bool> {
         Ok(path.is_file())
     }
 
@@ -124,13 +142,13 @@ impl FileAbstraction for FSFileAbstraction {
     }
 
     /// We return nothing from the FS here.
-    fn drain(&self) -> Result<Drain, SE> {
+    fn drain(&self) -> Result<Drain> {
         Ok(Drain::empty())
     }
 
     /// FileAbstraction::fill implementation that consumes the Drain and writes everything to the
     /// filesystem
-    fn fill(&mut self, mut d: Drain) -> Result<(), SE> {
+    fn fill(&mut self, mut d: Drain) -> Result<()> {
         d.iter()
             .fold(Ok(()), |acc, (path, element)| {
                 acc.and_then(|_| self.new_instance(path).write_file_content(&element))
@@ -141,7 +159,7 @@ impl FileAbstraction for FSFileAbstraction {
                           basepath: PathBuf,
                           storepath: PathBuf,
                           backend: Arc<FileAbstraction>)
-        -> Result<PathIterator, SE>
+        -> Result<PathIterator>
     {
         trace!("Building PathIterator object");
         Ok(PathIterator::new(Box::new(WalkDirPathIterBuilder { basepath }), storepath, backend))
@@ -153,13 +171,15 @@ pub(crate) struct WalkDirPathIterBuilder {
 }
 
 impl PathIterBuilder for WalkDirPathIterBuilder {
-    fn build_iter(&self) -> Box<Iterator<Item = Result<PathBuf, SE>>> {
+    fn build_iter(&self) -> Box<Iterator<Item = Result<PathBuf>>> {
         Box::new(WalkDir::new(self.basepath.clone())
             .min_depth(1)
             .max_open(100)
             .into_iter()
             .map(|r| {
-                r.map(|e| PathBuf::from(e.path())).chain_err(|| SE::from_kind(SEK::FileError))
+                r.map(|e| PathBuf::from(e.path()))
+                    .context(format_err!("Error in Walkdir"))
+                    .map_err(Error::from)
             }))
     }
 
@@ -168,8 +188,16 @@ impl PathIterBuilder for WalkDirPathIterBuilder {
     }
 }
 
-fn open_file<A: AsRef<Path>>(p: A) -> ::std::io::Result<File> {
-    OpenOptions::new().write(true).read(true).open(p)
+fn open_file<A: AsRef<Path>>(p: A) -> ::std::io::Result<Option<File>> {
+    match OpenOptions::new().write(true).read(true).open(p) {
+        Err(e) => {
+            match e.kind() {
+                ::std::io::ErrorKind::NotFound => return Ok(None),
+                _ => return Err(e),
+            }
+        },
+        Ok(file) => Ok(Some(file))
+    }
 }
 
 fn create_file<A: AsRef<Path>>(p: A) -> ::std::io::Result<File> {

lib/core/libimagstore/src/file_abstraction/iter.rs
@@ -20,7 +20,8 @@
 use std::path::PathBuf;
 use std::sync::Arc;
 
-use error::Result;
+use failure::Fallible as Result;
+
 use storeid::StoreId;
 use file_abstraction::FileAbstraction;
 

lib/core/libimagstore/src/file_abstraction/mod.rs
@@ -22,7 +22,8 @@ use std::fmt::Debug;
 use std::collections::HashMap;
 use std::sync::Arc;
 
-use error::StoreError as SE;
+use failure::Fallible as Result;
+
 use store::Entry;
 use storeid::StoreId;
 
@@ -38,20 +39,20 @@ use self::iter::PathIterator;
 
 /// An abstraction trait over filesystem actions
 pub trait FileAbstraction : Debug {
-    fn remove_file(&self, path: &PathBuf) -> Result<(), SE>;
-    fn copy(&self, from: &PathBuf, to: &PathBuf) -> Result<(), SE>;
-    fn rename(&self, from: &PathBuf, to: &PathBuf) -> Result<(), SE>;
-    fn create_dir_all(&self, _: &PathBuf) -> Result<(), SE>;
+    fn remove_file(&self, path: &PathBuf) -> Result<()>;
+    fn copy(&self, from: &PathBuf, to: &PathBuf) -> Result<()>;
+    fn rename(&self, from: &PathBuf, to: &PathBuf) -> Result<()>;
+    fn create_dir_all(&self, _: &PathBuf) -> Result<()>;
 
-    fn exists(&self, &PathBuf) -> Result<bool, SE>;
-    fn is_file(&self, &PathBuf) -> Result<bool, SE>;
+    fn exists(&self, &PathBuf) -> Result<bool>;
+    fn is_file(&self, &PathBuf) -> Result<bool>;
 
     fn new_instance(&self, p: PathBuf) -> Box<FileAbstractionInstance>;
 
-    fn drain(&self) -> Result<Drain, SE>;
-    fn fill<'a>(&'a mut self, d: Drain) -> Result<(), SE>;
+    fn drain(&self) -> Result<Drain>;
+    fn fill<'a>(&'a mut self, d: Drain) -> Result<()>;
 
-    fn pathes_recursively(&self, basepath: PathBuf, storepath: PathBuf, backend: Arc<FileAbstraction>) -> Result<PathIterator, SE>;
+    fn pathes_recursively(&self, basepath: PathBuf, storepath: PathBuf, backend: Arc<FileAbstraction>) -> Result<PathIterator>;
 }
 
 /// An abstraction trait over actions on files
@@ -61,8 +62,8 @@ pub trait FileAbstractionInstance : Debug {
     ///
     /// The `StoreId` is passed because the backend does not know where the Entry lives, but the
     /// Entry type itself must be constructed with the id.
-    fn get_file_content(&mut self, id: StoreId) -> Result<Entry, SE>;
-    fn write_file_content(&mut self, buf: &Entry) -> Result<(), SE>;
+    fn get_file_content(&mut self, id: StoreId) -> Result<Option<Entry>>;
+    fn write_file_content(&mut self, buf: &Entry) -> Result<()>;
 }
 
 pub struct Drain(HashMap<PathBuf, Entry>);
@@ -119,7 +120,7 @@ version = "{}"
 Hello World"#, env!("CARGO_PKG_VERSION"))).unwrap();
 
         lf.write_file_content(&file).unwrap();
-        let bah = lf.get_file_content(loca).unwrap();
+        let bah = lf.get_file_content(loca).unwrap().unwrap();
         assert_eq!(bah.get_content(), "Hello World");
     }
 
@@ -140,7 +141,7 @@ Hello World
 baz"#, env!("CARGO_PKG_VERSION"))).unwrap();
 
         lf.write_file_content(&file).unwrap();
-        let bah = lf.get_file_content(loca).unwrap();
+        let bah = lf.get_file_content(loca).unwrap().unwrap();
         assert_eq!(bah.get_content(), "Hello World\nbaz");
     }
 
@@ -163,7 +164,7 @@ baz
 "#, env!("CARGO_PKG_VERSION"))).unwrap();
 
         lf.write_file_content(&file).unwrap();
-        let bah = lf.get_file_content(loca).unwrap();
+        let bah = lf.get_file_content(loca).unwrap().unwrap();
         assert_eq!(bah.get_content(), "Hello World\nbaz\n\n");
     }
 

commit becfcd41807bf65fb6f8543800bb57d7e8e93d7a

Fix: In-Memory test backend: Actually remove the old entry on "move"
This fixes a really ugly bug where the in-memory backend for the store
did not remove the entry from the in-memory hashmap on "move", but
simply copied it from the old location to the new one.

That caused tests to fail after the fixes introduced for the
Store::get() function which checked the filesystem and the internal
cache whether an entry exists before the actual "get" operation, because
an old entry would still exist after a move (only in the testcases).

Signed-off-by: Matthias Beyer <mail@beyermatthias.de>
Fixes: 09e8619cf5 ("libimagstore: Move from error-chain to failure")

lib/core/libimagstore/src/file_abstraction/inmemory.rs
@@ -140,7 +140,7 @@ impl FileAbstraction for InMemoryFileAbstraction {
         let mut mtx = self.backend().lock().expect("Locking Mutex failed");
         let backend = mtx.get_mut();
 
-        let a = backend.get(from).cloned().ok_or_else(|| EM::FileNotFound)?;
+        let a = backend.remove(from).ok_or_else(|| EM::FileNotFound)?;
         backend.insert(to.clone(), a);
         debug!("Renaming: {:?} -> {:?} worked", from, to);
         Ok(())